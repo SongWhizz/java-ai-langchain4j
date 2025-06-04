945e5d0a7649c61b95e3357c37b9a67a53a578e3
SongWhizz <SongWhizz@noreply.gitcode.com>
2025-06-04 17:21:12 +0800

    预约业务的实现

---
+++ src/main/java/com/gxust/java/ai/langchain4j/mapper/AppointmentMapper.java
@@ -0,0 +1,9 @@
+package com.gxust.java.ai.langchain4j.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.gxust.java.ai.langchain4j.entity.Appointment;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface AppointmentMapper extends BaseMapper<Appointment> {
+}
---
+++ src/main/java/com/gxust/java/ai/langchain4j/service/AppointmentService.java
@@ -0,0 +1,8 @@
+package com.gxust.java.ai.langchain4j.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.gxust.java.ai.langchain4j.entity.Appointment;
+
+public interface AppointmentService extends IService<Appointment> {
+    Appointment getOne(Appointment appointment);
+}
