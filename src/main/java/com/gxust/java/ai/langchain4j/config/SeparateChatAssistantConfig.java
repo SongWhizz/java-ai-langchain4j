5c78d763883b0520686304d0a83081ef88a16a40
SongWhizz <SongWhizz@noreply.gitcode.com>
2025-06-04 11:37:35 +0800

    mongodbTest
    mongodb增删改

--- src/main/java/com/gxust/java/ai/langchain4j/config/SeparateChatAssistantConfig.java
+++ src/main/java/com/gxust/java/ai/langchain4j/config/SeparateChatAssistantConfig.java
@@ -2,6 +2,7 @@ package com.gxust.java.ai.langchain4j.config;
 
 import dev.langchain4j.memory.chat.ChatMemoryProvider;
 import dev.langchain4j.memory.chat.MessageWindowChatMemory;
+import dev.langchain4j.store.memory.chat.InMemoryChatMemoryStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +11,11 @@ public class SeparateChatAssistantConfig {
 
     @Bean
     public ChatMemoryProvider chatMemoryProvider(){
-        return memoryId -> MessageWindowChatMemory.builder().id(memoryId).maxMessages(10).build();
+        return memoryId -> MessageWindowChatMemory
+                .builder()
+                .id(memoryId)
+                .maxMessages(10)
+                .chatMemoryStore(new InMemoryChatMemoryStore())
+                .build();
     }
 }
