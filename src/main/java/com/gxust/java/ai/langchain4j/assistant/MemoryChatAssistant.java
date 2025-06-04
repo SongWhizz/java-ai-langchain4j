19d8eb36de02785cd49bc13ec9cc56a05a6abe18
SongWhizz <SongWhizz@noreply.gitcode.com>
2025-06-04 15:39:36 +0800

    用户提示词
    配置@UserMessage

--- src/main/java/com/gxust/java/ai/langchain4j/assistant/MemoryChatAssistant.java
+++ src/main/java/com/gxust/java/ai/langchain4j/assistant/MemoryChatAssistant.java
@@ -1,6 +1,7 @@
 package com.gxust.java.ai.langchain4j.assistant;
 
 
+import dev.langchain4j.service.UserMessage;
 import dev.langchain4j.service.spring.AiService;
 import dev.langchain4j.service.spring.AiServiceWiringMode;
 
@@ -10,5 +11,7 @@ import dev.langchain4j.service.spring.AiServiceWiringMode;
         chatMemory = "chatMemory"
 )
 public interface MemoryChatAssistant {
+
+    @UserMessage("你是我的好朋友，请用上海话回答问题，并且添加一些表情符号。 {{it}}") //{{it}}表示这里唯一的参数的占位符
     String chat(String message);
 }
--- src/test/java/com/gxust/java/ai/langchain4j/PromptTest.java
+++ src/test/java/com/gxust/java/ai/langchain4j/PromptTest.java
@@ -1,5 +1,6 @@
 package com.gxust.java.ai.langchain4j;
 
+import com.gxust.java.ai.langchain4j.assistant.MemoryChatAssistant;
 import com.gxust.java.ai.langchain4j.assistant.SeparateChatAssistant;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +15,16 @@ public class PromptTest {
     public void testSystemMessage() {
         String answer = separateChatAssistant.chat(4,"今天多少号？");
         System.out.println(answer);
+    }
+
+    @Autowired
+    private MemoryChatAssistant memoryChatAssistant;
+    @Test
+    public void testUserMessage(){
+        String answer1 = memoryChatAssistant.chat("我是hansong");
+        System.out.println(answer1);
+        String answer2 = memoryChatAssistant.chat("你能推荐一些互联网的实习给我吗？");
+        System.out.println(answer2);
     }
 
 }
