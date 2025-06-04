a01c4b2b7094b1ec78a15e2bb7cffcd502a06032
SongWhizz <SongWhizz@noreply.gitcode.com>
2025-06-04 15:33:32 +0800

    系统提示词
    配置@SystemMessage

--- src/main/java/com/gxust/java/ai/langchain4j/assistant/SeparateChatAssistant.java
+++ src/main/java/com/gxust/java/ai/langchain4j/assistant/SeparateChatAssistant.java
@@ -1,6 +1,7 @@
 package com.gxust.java.ai.langchain4j.assistant;
 
 import dev.langchain4j.service.MemoryId;
+import dev.langchain4j.service.SystemMessage;
 import dev.langchain4j.service.UserMessage;
 import dev.langchain4j.service.spring.AiService;
 import dev.langchain4j.service.spring.AiServiceWiringMode;
@@ -11,5 +12,7 @@ import dev.langchain4j.service.spring.AiServiceWiringMode;
         chatMemoryProvider = "chatMemoryProvider"
 )
 public interface SeparateChatAssistant {
+
+    @SystemMessage(fromResource = "my-prompt-template.txt")//系统消息提示词
     String chat(@MemoryId int memoryId, @UserMessage String userMessage);
 }
---
+++ src/test/java/com/gxust/java/ai/langchain4j/PromptTest.java
@@ -0,0 +1,19 @@
+package com.gxust.java.ai.langchain4j;
+
+import com.gxust.java.ai.langchain4j.assistant.SeparateChatAssistant;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class PromptTest {
+
+    @Autowired
+    private SeparateChatAssistant separateChatAssistant;
+    @Test
+    public void testSystemMessage() {
+        String answer = separateChatAssistant.chat(4,"今天多少号？");
+        System.out.println(answer);
+    }
+
+}
